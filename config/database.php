5c6dd8fd537f5853a65d4d5389ee83f7256a4170
Burrynter <burrynter@gmail.com>
2018-03-17 21:40:51 +0200

    db+mail on zzz

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'mysql.zzz.com.ua'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'caerwyn'),
+            'username' => env('DB_USERNAME', 'Caerwyn'),
+            'password' => env('DB_PASSWORD', 'Ca57er21wyn512'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', 'mail.zzz.com.ua'),
 
     /*
     |--------------------------------------------------------------------------
@@ -42,7 +42,7 @@ return [
     |
     */
 
-    'port' => env('MAIL_PORT', 587),
+    'port' => env('MAIL_PORT', 2525),
 
     /*
     |--------------------------------------------------------------------------
@@ -84,9 +84,9 @@ return [
     |
     */
 
-    'username' => env('MAIL_USERNAME'),
+    'username' => env('caerwyn@laravelblog.zzz.com.ua'),
 
-    'password' => env('MAIL_PASSWORD'),
+    'password' => env('Ca57er21wyn512'),
 
     /*
     |--------------------------------------------------------------------------
